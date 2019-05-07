cb0e0a8e9469f8883871f2be65e2bdd969edf32f
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-05-07 10:43:06 -0700

    lab completed

--- counter_app/src/App.js
+++ counter_app/src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <h1>Counter App</h1>
+    </header>
   );
+};
+
+class Content extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      number: 0,
+    };
+  }
+
+  handleWord = e => {
+    let number = e.target.value;
+    this.setState({ number });
+  };
+
+  handleClickPositive = e => {
+    e.preventDefault();
+    let number = this.state.number
+      number++
+    this.setState({ number });
+  };
+
+  handleClickNegative = e => {
+    e.preventDefault();
+    let number = this.state.number
+      number--
+    this.setState({ number });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.number}</p>
+        <button onClick={this.handleClickPositive}>+</button>
+        <button onClick={this.handleClickNegative}>-</button>
+      </div>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Header />
+        <Content />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
